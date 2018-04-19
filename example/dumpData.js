a9e0b73f7b7fb56ac5f295b2cd4dac80c6b00da4
kael fischer <kael@ubiota.com>
2018-04-19 14:03:44 -0600

    dumpData example

--- example/dumpData.js
+++ example/dumpData.js
@@ -11,8 +11,8 @@ const cr0 = sensor.cr0Register();
 const cr1 = sensor.cr1Register();
 const faultByte = sensor.faultRegister();
 
-console.log('CR0: ' + Number((cr0 & 0b10000000)>0).toString());
-console.log('CMODE: ' + Number(cr0).toString(2));
+console.log('CR0: ' + Number(cr0 ).toString(2));
+console.log('CMODE: ' + Number((cr0 & 0b10000000)>0).toString());
 
 console.log('CR1: ' + Number(sensor.cr1Register()).toString(2));
 console.log('Thermocouple Type: ' + sensor.tcType());
